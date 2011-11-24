e5566eecef3b521daa3f9c80134113af356dc0b4
Anatoli Makarevich <makaroni4@gmail.com>
2011-11-24 16:28:12 +0300

    Exclude mongo ORMs

--- lib/rails3-jquery-autocomplete/autocomplete.rb
+++ lib/rails3-jquery-autocomplete/autocomplete.rb
@@ -2,13 +2,7 @@ module Rails3JQueryAutocomplete
   module Autocomplete
     def self.included(target)
       target.extend Rails3JQueryAutocomplete::Autocomplete::ClassMethods
-
-      if defined?(Mongoid::Document)
-        target.send :include, Rails3JQueryAutocomplete::Orm::Mongoid
-      elsif defined?(MongoMapper::Document)
-        target.send :include, Rails3JQueryAutocomplete::Orm::MongoMapper
-      else
-        target.send :include, Rails3JQueryAutocomplete::Orm::ActiveRecord
+      target.send :include, Rails3JQueryAutocomplete::Orm::ActiveRecord
       end
     end
 
