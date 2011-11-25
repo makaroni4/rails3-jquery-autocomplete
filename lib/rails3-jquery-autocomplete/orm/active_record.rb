106f8ae34f05bec7e16f8da0cb5a8ac60a83b55d
Anatoli Makarevich <makaroni4@gmail.com>
2011-11-25 11:21:09 +0300

    Added "globalized" parameter for the case if needed field is in  separate table included by scope
    I use globalize3 gem and need field "name" which is in the "model_translations" table and I need skip model name in query to search exactly in translations

--- lib/rails3-jquery-autocomplete/orm/active_record.rb
+++ lib/rails3-jquery-autocomplete/orm/active_record.rb
@@ -11,16 +11,14 @@ module Rails3JQueryAutocomplete
 
       def get_autocomplete_items(parameters)
         model   = parameters[:model]
-        term    = CGI.unescape(parameters[:term])
+        term    = parameters[:term]
         method  = parameters[:method]
         options = parameters[:options]
         scopes  = Array(options[:scopes])
         limit   = get_autocomplete_limit(options)
         order   = get_autocomplete_order(method, options, model)
-
-
+        
         items = model.scoped
-
         scopes.each { |scope| items = items.send(scope) } unless scopes.empty?
 
         items = items.select(get_autocomplete_select_clause(model, method, options)) unless options[:full_model]
@@ -30,14 +28,16 @@ module Rails3JQueryAutocomplete
 
       def get_autocomplete_select_clause(model, method, options)
         table_name = model.table_name
-        (["#{table_name}.#{model.primary_key}", "#{table_name}.#{method}"] + (options[:extra_data].blank? ? [] : options[:extra_data]))
+        primary_key = model.primary_key
+        (["#{table_name}.#{primary_key}", "#{table_name}.#{method}"] + (options[:extra_data].blank? ? [] : options[:extra_data]))
       end
 
       def get_autocomplete_where_clause(model, term, method, options)
         table_name = model.table_name
         is_full_search = options[:full]
         like_clause = (postgres? ? 'ILIKE' : 'LIKE')
-        ["LOWER(#{table_name}.#{method}) #{like_clause} ?", "#{(is_full_search ? '%' : '')}#{term.downcase}%"]
+        search_field = options[:globalized] ? method : "#{table_name}.#{method}"
+        ["LOWER(#{search_field}) #{like_clause} ?", "#{(is_full_search ? '%' : '')}#{term.downcase}%"]
       end
 
       def postgres?
